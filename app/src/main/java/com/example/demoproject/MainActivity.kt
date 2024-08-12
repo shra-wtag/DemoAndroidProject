a9b316e72308a3da7b6651df5e3bb7c7ced0b055
ksh-wtag <shakawat.ratul@welldev.io>
2024-08-12 15:40:28 +0600

    removed comment

--- app/src/main/java/com/example/demoproject/MainActivity.kt
+++ app/src/main/java/com/example/demoproject/MainActivity.kt
@@ -18,14 +18,10 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
-    }
-
-    override fun onStart() {
-        super.onStart()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
